ec1085554472b713c2e0ca24e7428bfe02693881
zhaozhiyangzzy <zzybry@163.com>
2022-06-12 17:53:15 +0800

    app.py home page

---
+++ watchlist/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'hello'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User:%s' % name
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    return 'Test page'
+@app.route('/home')
+def hello():
+    return 'welcome to my watchlist'
